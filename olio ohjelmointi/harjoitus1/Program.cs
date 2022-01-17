f44ec8a7ddf88395f42325103000e4682daa8c47
Aldrinzog <aldrin.zogijani@student.careeria.fi>
2022-01-17 13:28:17 +0200

    projekti luotu

---
+++ olio ohjelmointi/harjoitus1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
