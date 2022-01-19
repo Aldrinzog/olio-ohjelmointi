ac0e3011ea178a360765fbecfd3aed8ee31a14b0
Aldrinzog <aldrin.zogijani@student.careeria.fi>
2022-01-19 09:41:31 +0200

    Kiuas tehty

---
+++ olio ohjelmointi/kiuas/Kiuas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kiuas
+{
+    class Kiuas
+    {
+        public string Lampotila;
+        public string Merkki;
+        public string Kosteus;
+        public string Tila;
+
+        public Kiuas(string merkki, int Lampotila, int kosteus)
+        {
+            Merkki = Merkki;
+            Lampotila = Lampotila;
+            int = kosteus;
+            Tila = false; // Tila alussa on aina false!
+        }
+
+
+        public void HaeTiedot()
+        {
+            Console.WriteLine("");
+            if (Tila == true)
+            {
+                Console.WriteLine("Kiuas" + Merkki + "on päällä");
+                Console.WriteLine("Lampotila" + Lampotila);
+                Console.WriteLine("kosteus" + Kosteus);
+                Console.WriteLine("tila" + Tila);
+            }
+            else
+            {
+                Console.WriteLine("Kiuas " + Merkki + " ei ole päällä");
+            }
+        }
+    }
---
+++ olio ohjelmointi/kiuas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace kiuas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas harvia = new Kiuas("harvia", 80, 50);
+
+            harvia.HaeTiedot();
+            harvia.Tila = true;
+            harvia.HaeTiedot();
+
+            harvia.Lampotila = 90;
+            harvia.Kosteus = 60;
+
+            harvia.HaeTiedot();
+            harvia.Tila = false;
+            harvia.HaeTiedot();
+        }
+    }
+}
