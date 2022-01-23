3aeb349d7258f506337e6451f0178ee78cf2cfe8
Aldrinzog <aldrin.zogijani@student.careeria.fi>
2022-01-23 14:48:49 +0200

    Harjoitus 5 + staattinen testi

---
+++ olio ohjelmointi/Harjoitus5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Harjoitus5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 5.0f;
+            float b = 2.5f;
+
+            float summa = laskin.summa(a, b);
+            float Erotus = laskin.Erotus(a, b);
+            float Kertolasku = laskin.kertolasku(a, b);
+            float jakolasku = laskin.Jakolasku(a, b);
+
+            Console.WriteLine("Numeroiden {0} ja {1}":, a, b);
+            Console.WriteLine("Summa: " + summa);
+            Console.WriteLine("Erotus: " + Erotus);
+            Console.WriteLine("Kertolasku: " + Kertolasku);
+            Console.WriteLine("Jakolasku: " + jakolasku);
+
+                
+           
+        }
+    }
+}
---
+++ olio ohjelmointi/Harjoitus5/laskin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus5
+{
+   static  class laskin
+   {
+        public static float summa(float a, float b)
+        {
+            return a + b;
+        }
+
+
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+
+
+        public static float kertolasku(float a, float b)
+        {
+            return a - b;
+        }
+
+
+        public static float Jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
